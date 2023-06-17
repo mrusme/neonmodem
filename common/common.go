df1594d2cdfe4b9f03d0d847d8ef1d77ef800a88
tedwardd <ted@xy0.org>
2023-06-16 21:03:41 -0400

    sync local changes upstream

--- common/common.go
+++ common/common.go
@@ -1,8 +1,19 @@
 package common
 
 import (
+	"fmt"
+
+	"github.com/99designs/keyring"
 	"github.com/manifoldco/promptui"
 )
+
+type PasswordError struct {
+	Reason string
+}
+
+func (e *PasswordError) Error() string {
+	return fmt.Sprintf("reason: %v", e.Reason)
+}
 
 func YesNo() (bool, error) {
 	prompt := promptui.Select{
@@ -17,4 +28,36 @@ func YesNo() (bool, error) {
 
 	return result == "Yes", nil
 
+}
+
+func SetPassword(pwd []byte) (string, error) {
+	if len(pwd) < 0 {
+		return "", &PasswordError{
+			Reason: "Password can not be empty",
+		}
+	}
+	fmt.Println(string(pwd))
+
+	// Open system keyring object
+	ring, _ := keyring.Open(keyring.Config{
+		ServiceName: "NeonModem - Lemmy",
+	})
+
+	// Attempt to save the password to system keyring
+	// If we can't, ask if should save it in clear text
+	err := ring.Set(keyring.Item{
+		Key:  "password",
+		Data: pwd,
+	})
+	if err != nil {
+		fmt.Println("Unable to save password to a keyring. Would you like to proceed to save the password in clear text in the neonmodem.toml?")
+		if resp, _ := YesNo(); resp != true {
+			fmt.Println("Not adding lemmy account...")
+			return "", &PasswordError{
+				Reason: "Keyring unavailable",
+			}
+		}
+	}
+
+	return "password_in_keyring", nil
 }
--- system/lemmy/connect.go
+++ system/lemmy/connect.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/99designs/keyring"
 	"github.com/mrusme/neonmodem/common"
 	"golang.org/x/term"
 )
@@ -27,43 +26,19 @@ func (sys *System) Connect(sysURL string) error {
 		}
 	}
 
-	// Request input from user
-	var password string = ""
-	for password == "" {
-		fmt.Printf(
-			"Please enter your password (will not echo): ",
-		)
-		bytepw, err := term.ReadPassword(int(syscall.Stdin))
-		fmt.Println("")
-		if err != nil || len(bytepw) == 0 {
-			fmt.Println("Invalid input")
-		}
-		password = string(bytepw)
-	}
-
 	// Credentials
 	credentials := make(map[string]string)
 	credentials["username"] = username
 
-	ring, _ := keyring.Open(keyring.Config{
-		ServiceName: "NeonModem - Lemmy",
-	})
-	// Attempt to save the password to system keyring
-	err := ring.Set(keyring.Item{
-		Key:  "password",
-		Data: []byte(password),
-	})
-	// On failure, prompt to continue with insecure password storage or abort
-	if err != nil {
-		fmt.Println("Unable to save password to a keyring. Would you like to proceed to save the password in clear text in the neonmodem.toml?")
-		if resp, _ := common.YesNo(); resp != true {
-			fmt.Println("Not adding lemmy account...")
-			os.Exit(0)
-		} else {
+	// New password code
+	for {
+		fmt.Println("Please enter your password (will not echo): ")
+		bytepw, err := term.ReadPassword(int(syscall.Stdin))
+		password, err := common.SetPassword(bytepw)
+		if err == nil {
 			credentials["password"] = password
+			break
 		}
-	} else {
-		credentials["password"] = "password_in_keyring"
 	}
 
 	if sys.config == nil {
